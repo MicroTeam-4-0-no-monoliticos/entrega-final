7885bd7247ab5af8b15dcf664ccb7dd732bea478
HectorFranco-MISO <h.franco@uniandes.edu.co>
2025-09-07 00:39:43 -0500

    First module under construction

---
+++ src/aeropartners/api/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+from flask import Flask, render_template, request, url_for, redirect, jsonify, session
+from flask_swagger import swagger
+
+# Identifica el directorio base
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+def create_app(config={}):
+    pass
---
+++ src/aeropartners/api/commision.py
@@ -0,0 +1,3 @@
+import aeropartners.seedwork.presentation.api as api
+
+bp = api.crear_blueprint('commision', '/commision')
