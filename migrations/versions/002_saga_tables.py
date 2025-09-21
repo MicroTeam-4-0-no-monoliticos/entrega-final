9f45c43c370e5f136e29b91796d78e3f81db6178
Juan Sebastian Vargas Quintero <js.vargasq1@uniandes.edu.co>
2025-09-20 22:26:45 -0500

    Upload DB configuration

--- migrations/versions/002_saga_tables.py
+++ migrations/versions/002_saga_tables.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = '002_saga_tables'
-down_revision = '001_initial_migration'
+revision = '002'
+down_revision = '001'
 branch_labels = None
 depends_on = None
 
